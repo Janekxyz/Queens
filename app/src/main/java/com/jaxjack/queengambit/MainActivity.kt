2afa30d535bc41ab59aa436b52d3b0da427c0605
Kacper <kacperjankowski@hotmail.com>
2026-08-10 12:41:54 +0200

    Add MainActivity.kt

---
+++ app/src/main/java/com/jaxjack/queengambit/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.jaxjack.queengambit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jaxjack.queengambit.styleguide.theme.QueenGambitTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QueenGambitTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding).padding(16.dp)) {
+                        Greeting(name = "Queen Gambit")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QueenGambitTheme {
+        Greeting("Queen Gambit")
+    }
+}
